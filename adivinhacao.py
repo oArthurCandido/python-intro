aa3af61eabc2159725db2432379593ebf20855b5
Arthur Candido <oarthurcandido@gmail.com>
2022-12-14 20:56:22 -0300

    :tada: init: first steps with python

---
+++ adivinhacao.py
@@ -0,0 +1,40 @@
+'''
+Jogo de adivinhação
+'''
+print("*********************************")
+print("Bem vindo ao jogo de adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+# chute = input("Digite o seu numero: ")
+
+# print("Você digitou ", chute)
+RODADA = 1
+
+# numero = int(chute)
+
+# if (numero_secreto == numero):
+#     print("Você acertou!")
+# else:
+#     print("Você errou!")
+
+# print("Fim do jogo")
+
+minha_idade = 26
+idade_namorado = 26
+if (minha_idade == idade_namorado):
+    print('temos idades iguais')
+else:
+    print('temos idades diferentes')
+
+
+numero1 = 10
+numero2 = 10
+if (numero1 == numero2):
+    print("São números iguais")
+
+
+nome = "Nico"
+sobrenome = "Steppat"
+print(nome + sobrenome)
---
+++ app.py
@@ -0,0 +1,12 @@
+"""
+Some tests
+"""
+DIA = 10
+MES = 10
+ANO = 2010
+NOME1 = 'Arthur'
+NOME2 = 'João'
+QUAT = 5
+print(type(QUAT))
+print(NOME1, NOME2, sep='-')
+print(DIA, MES, ANO, sep='/')
