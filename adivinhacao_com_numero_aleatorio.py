859069471d72b47996364c0763bbfe6701fcdb05
Arthur Candido <oarthurcandido@gmail.com>
2022-12-18 19:29:39 -0300

    :sparkles: feat: random number

--- adivinhacao_com_numero_aleatorio.py
+++ adivinhacao_com_numero_aleatorio.py
@@ -11,33 +11,32 @@ def guess_number():
 
 TENTATIVAS = 10
 rodada = 1
-NUMERO_SECRETO = random.randrange(9, 10)
+NUMERO_SECRETO = random.randrange(1, 100)
 
-print(NUMERO_SECRETO)
 
-# for rodada in range(1, TENTATIVAS + 1):
-#     print("Tentativa {} de {}".format(rodada,  TENTATIVAS))
+for rodada in range(1, TENTATIVAS + 1):
+    print("Tentativa {} de {}".format(rodada,  TENTATIVAS))
 
-#     chute = input("Digite o seu numero: ")
-#     numero = int(chute)
-#     if (numero < 1 or numero > 100):
-#         print("Você deve digitar um número entre 1 e 100!")
+    chute = input("Digite o seu numero: ")
+    numero = int(chute)
+    if (numero < 1 or numero > 100):
+        print("Você deve digitar um número entre 1 e 100!")
 
-#         continue
-#     acertou = NUMERO_SECRETO == numero
-#     maior = NUMERO_SECRETO < numero
-#     menor = NUMERO_SECRETO > numero
+        continue
+    acertou = NUMERO_SECRETO == numero
+    maior = NUMERO_SECRETO < numero
+    menor = NUMERO_SECRETO > numero
 
-#     if acertou:
-#         print("Você acertou!")
-#         break
+    if acertou:
+        print("Você acertou!")
+        break
 
-#     elif maior:
-#         print("Você errou pra mais!", )
+    elif maior:
+        print("Você errou pra mais!", )
 
-#     elif menor:
-#         print("Você errou pra menos!")
-#     rodada = rodada + 1
+    elif menor:
+        print("Você errou pra menos!")
+    rodada = rodada + 1
 
-# print("Fim de jogo, o palpite correto era {}!".format(NUMERO_SECRETO))
-# guess_number()
+print("Fim de jogo, o palpite correto era {}!".format(NUMERO_SECRETO))
+guess_number()
