acef36b4f85eb9455c20898624b918aaf11bd2d7
Arthur Candido <oarthurcandido@gmail.com>
2022-12-18 19:28:54 -0300

    :sparkles: feat: random number

---
+++ adivinhacao_com_numero_aleatorio.py
@@ -0,0 +1,43 @@
+""" Random number guessing game """
+
+import random
+
+"""laço for"""
+
+
+def guess_number():
+    """Guess the number"""
+
+
+TENTATIVAS = 10
+rodada = 1
+NUMERO_SECRETO = random.randrange(9, 10)
+
+print(NUMERO_SECRETO)
+
+# for rodada in range(1, TENTATIVAS + 1):
+#     print("Tentativa {} de {}".format(rodada,  TENTATIVAS))
+
+#     chute = input("Digite o seu numero: ")
+#     numero = int(chute)
+#     if (numero < 1 or numero > 100):
+#         print("Você deve digitar um número entre 1 e 100!")
+
+#         continue
+#     acertou = NUMERO_SECRETO == numero
+#     maior = NUMERO_SECRETO < numero
+#     menor = NUMERO_SECRETO > numero
+
+#     if acertou:
+#         print("Você acertou!")
+#         break
+
+#     elif maior:
+#         print("Você errou pra mais!", )
+
+#     elif menor:
+#         print("Você errou pra menos!")
+#     rodada = rodada + 1
+
+# print("Fim de jogo, o palpite correto era {}!".format(NUMERO_SECRETO))
+# guess_number()
--- formatacoes.py
+++ formatacoes.py
@@ -5,3 +5,6 @@ MES = 1
 ANO = 2019
 
 print("Data: {:02d}/{:02d}/{}".format(DIA, MES, ANO))
+
+
+print("R$ {:7.1f}".format(1234.50))
