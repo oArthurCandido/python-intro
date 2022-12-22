4785d739436b89d798dbb31ebb7b4d5229f4d116
Arthur Candido <oarthurcandido@gmail.com>
2022-12-22 15:57:58 -0300

    :construction: WIP: advancing forca game

--- avancando_na_linguagem/forca.py
+++ avancando_na_linguagem/forca.py
@@ -4,20 +4,23 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana".upper()
+    letras_acertadas = ['_' for _ in range(len(palavra_secreta))]
 
     enforcou = False
     acertou = False
 
     while (not acertou and not enforcou):
+
         chute = input("Qual letra? ")
         chute = chute.strip().upper()
 
         index = 0
         for letra in palavra_secreta:
             if chute == letra:
-                print("Encontrei a leytra {} na posição {}".format(letra, index))
-                index = index + 1
-        print("Jogando")
+                letras_acertadas[index] = letra
+            index = index + 1
+
+        print(letras_acertadas)
 
 
 if __name__ == "__main__":
